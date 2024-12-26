8e53c0041a06698eb1bdf13fca360dc7f9f5e27c
KesseNones <jesse.jones@wsu.edu>
2024-12-26 03:01:37 -0800

    Ported Lmao's attempt onError code to lmaoc

--- lmaoc.rs
+++ lmaoc.rs
@@ -1,6 +1,6 @@
 //Jesse A. Jones
 //lmaoc the Lmao Compiler
-//Version: 0.8.0
+//Version: 0.8.1
 
 use std::collections::HashMap;
 use std::env;
@@ -283,7 +283,8 @@ enum ASTNode{
     Function{func_cmd: String, func_name: String, func_bod: Box<ASTNode>},
     Variable{var_name: String, var_cmd: String, var_num: usize},
     LocVar{name: String, cmd: String, num: usize},
-    BoxOp(String)
+    BoxOp(String),
+    AttErr{attempt: Box<ASTNode>, err: Box<ASTNode>},
 }
 
 impl Default for ASTNode{
@@ -308,6 +309,7 @@ impl fmt::Display for ASTNode{
             ASTNode::Variable{var_name: name, var_cmd: cmd, var_num: n} => write!(f, "Variable [name: {}, cmd: {}, num: {}]", name, cmd, n),
             ASTNode::LocVar{name: nm, cmd: c, num: n} => write!(f, "Local Variable [name: {}, cmd: {}, num: {}]", nm, c, n),
             ASTNode::BoxOp(op) => write!(f, "BoxOp {}", op),
+            ASTNode::AttErr{attempt: att, err: e} => write!(f, "AttErr [attempt: {}, err: {}]", att, e),
         }
     }
 }
@@ -336,6 +338,7 @@ impl Clone for ASTNode{
             },
             ASTNode::LocVar{name: nam, cmd: c, num: n} => ASTNode::LocVar{name: nam.clone(), cmd: c.clone(), num: *n},
             ASTNode::BoxOp(op) => ASTNode::BoxOp(op.clone()),
+            ASTNode::AttErr{attempt: att, err: e} => ASTNode::AttErr{attempt: att.clone(), err: e.clone()},
         }
     }
 }
@@ -801,6 +804,13 @@ fn make_ast_prime(
                     panic!("Malformed box command! No box command token given!")
                 }
             },
+            //Attempt onError case.
+            Token::Word(ref cmd) if cmd.0 == "attempt" => {
+                let (att_branch, err_branch, tokens_prime, token_index_prime) = 
+                    parse_att_err(tokens, token_index + 1, loc_nums, curr_loc_num);
+                already_parsed.push(ASTNode::AttErr{attempt: Box::new(att_branch), err: Box::new(err_branch)});
+                make_ast_prime(already_parsed, tokens_prime, token_index_prime, loc_nums, curr_loc_num, terminators)
+            },
             _ => {
                 let mut toks = tokens;
                 already_parsed.push(ASTNode::Terminal(std::mem::take(&mut toks[token_index])));
@@ -809,6 +819,42 @@ fn make_ast_prime(
         }
     }
 
+}
+
+//Used to recursively parse an attempt branch for AttErr
+fn parse_att_err(
+    tokens: Vec<Token>,
+    token_index: usize, 
+    loc_nums: &mut HashMap<String, usize>,
+    curr_loc_num: &mut usize) -> (ASTNode, ASTNode, Vec<Token>, usize){
+    let (att_branch, tokens_prime, token_index_prime, terminator_index) = 
+        make_ast_prime(
+            Vec::new(),
+            tokens, 
+            token_index, loc_nums, curr_loc_num,
+            vec![Token::Word(("onError".to_string(), 0))]
+        );
+    match terminator_index{
+        Some(i) => {
+            match tokens_prime[i]{
+                Token::Word(ref cmd) if cmd.0 == "onError" => {
+                    let (error_branch, tokens_prime_prime, token_index_prime_prime, _) = 
+                        make_ast_prime(
+                            Vec::new(),
+                            tokens_prime,
+                            token_index_prime, 
+                            loc_nums,
+                            curr_loc_num,
+                            vec![Token::Word((";".to_string(), 0))]
+                        );
+                    (ASTNode::Expression(att_branch), ASTNode::Expression(error_branch), 
+                        tokens_prime_prime, token_index_prime_prime)
+                },
+                _ => panic!("Failed to correctly construct attempt onError block!"),
+            }
+        },
+        None => panic!("REALLY SHOULD NEVER GET HERE!")
+    }
 }
 
 fn parse_if(
@@ -1076,6 +1122,35 @@ fn translate_ast_to_rust_code(
                             }}
                         ", &nm, &c, n);
                         code_strings.push(code_str)
+                    },
+                    ASTNode::AttErr{attempt: att, err: e} => {
+                        let attempt_code = make_code_str_from_ast(&att, ops_to_funcs);
+                        let error_code = make_code_str_from_ast(&e, ops_to_funcs);
+
+                        let code_str = format!("
+                            {{
+                                add_frame(state);
+                                let att_func = |state: &mut State| -> Result<(), String> {{
+                                    {}
+                                }};
+                                match att_func(state){{
+                                    Ok(_) => (),
+                                    Err(e) => {{
+                                        let new_bn = state.insert_to_heap(HeapValue::String(e));
+                                        state.stack.push(Value::StringBox(new_bn));
+                                        add_frame(state);
+                                        let err_func = |state: &mut State| -> Result<(), String> {{
+                                            {}
+                                        }};
+                                        match err_func(state){{
+                                            Ok(_) => (),
+                                            Err(e) => return error_and_remove_frame(state, e),
+                                        }}
+                                    }},
+                                }}
+                            }}
+                        ", attempt_code, error_code);
+                        code_strings.push(code_str);
                     },
                     _ => {},
                 }
